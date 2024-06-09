a41f5ac90df9a86f7aa83defa831515178302dbc
Michele Porta <m.porta@doveconviene.com>
2024-06-09 23:34:21 +0200

    Change logic

--- interface.py
+++ interface.py
@@ -6,19 +6,18 @@ DEFAULT_LOCATION= "Torino"
 
 def satellites():
     print (f"Call API with location {location}")
-    if st.session_state.location:
-        location = st.session_state.location
-        if (len(location) == 0):
-            #using default location
-            location= DEFAULT_LOCATION
+    if 'location' not in st.session_state:
+        st.session_state.location = DEFAULT_LOCATION
     else:
-        location= DEFAULT_LOCATION
-    address,lat,lng = get_coordinates(location)
-    satellites_info= get_satellites(lat,lng)
-    n_of_satellites= satellites_info['info']['satcount']
+        if (len(st.session_state.location) == 0):
+            #using default location if user done an empty search
+            location= DEFAULT_LOCATION
+    address,lat,lng = get_coordinates(st.session_state.location)
+    satellites_info = get_satellites(lat,lng)
+    n_of_satellites = satellites_info['info']['satcount']
     df = pd.json_normalize(satellites_info['above'])
-    df['satid']= df['satid'].apply(str)
-    df['info']= 'https://www.n2yo.com/satellite/?s='+ df['satid']
+    df['satid'] = df['satid'].apply(str)
+    df['info'] = 'https://www.n2yo.com/satellite/?s='+ df['satid']
     st.write("Your (more detailed) location:", address)
     st.subheader(f":satellite: Number of satellites over your head: {n_of_satellites}")
     st.write("Complete list of satellites")
